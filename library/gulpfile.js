f83f82d5709f3be9906afb252bc1d0cf454bef21
Mike Chacon <mike@sicdigital.com>
2016-03-27 15:29:27 +0700

    Added gulp watch task, to automatically watch scss files

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
 
 gulp.task('default', ['sass']);
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/**/*.scss', ['sass']);
+});
 
 gulp.task('sass', function () {
     gulp.src('./scss/style.scss')
